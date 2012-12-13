0109be5237ae5b7572e663e4a3db643c8c04e8e5
Chao San-Hsu <shanhsu@Chaos-MacBook-Pro.local>
2012-12-13 10:46:11 +0800

    Initial Commit

--- src/com/example/adonitsppsample/DrawView.java
+++ src/com/example/adonitsppsample/DrawView.java
@@ -1,116 +1,139 @@
 package com.example.adonitsppsample;
 import java.util.ArrayList;
-import java.util.List;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
 import android.graphics.Path;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class DrawView extends View implements OnTouchListener {
-	private static final String TAG = "DrawView";
 
-    List<MyPath> path = new ArrayList<MyPath>();
-    Paint paint = new Paint();
-    //Paint paintRed = new Paint();
-    int miColor = 0;
-    float mfPenWidth = 3;
-    private MyPath myPath;
-    
-	public DrawView(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
-		setFocusable(true);
-        setFocusableInTouchMode(true);
+class DrawView extends SurfaceView implements SurfaceHolder.Callback 
+{
+	private DrawingThread _thread;	
+	private Path path;
+	private ArrayList _graphics = new ArrayList();
+	private Paint mPaint;
 
-        this.setOnTouchListener(this);
-
-        paint.setDither(true);
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStrokeWidth(3);
-        paint.setDither(true);
-	}
-	
-	public void SetColor(int iColor)
+	public DrawView(Context context) 
 	{
-		miColor = iColor;
+	    super(context);
+		getHolder().addCallback(this);
+		_thread = new DrawingThread(getHolder(), this);
+		mPaint = new Paint();
+	    mPaint.setDither(true);
+	    mPaint.setColor(0xFFFF0000);
+	    mPaint.setStyle(Paint.Style.STROKE);
+	    mPaint.setStrokeJoin(Paint.Join.ROUND);
+	    mPaint.setStrokeCap(Paint.Cap.ROUND);
+	    mPaint.setStrokeWidth(3);
 	}
-	
-	public void SetStrokeWidth(float fWidth)
-	{
-		mfPenWidth = (float)fWidth;
-		Log.d(TAG, "mfPenWidth:" + String.valueOf(mfPenWidth));
-	}
-	
+
 	@Override
-    public void onDraw(Canvas canvas) {
-		for (MyPath onePath : path) {
-			switch (onePath.color) 
-			{
-				case 0:
-					paint.setColor(Color.BLACK);
-					paint.setStrokeWidth(onePath.fWidth);
-					canvas.drawPath(onePath, paint);
-					break;
-				case 1:
-					paint.setColor(Color.RED);
-					paint.setStrokeWidth(onePath.fWidth);
-					canvas.drawPath(onePath, paint);
-					break;
-				case 2:
-					paint.setColor(Color.GREEN);
-					paint.setStrokeWidth(onePath.fWidth);
-					canvas.drawPath(onePath, paint);
-					break;
-				case 3:
-					paint.setColor(Color.YELLOW);
-					paint.setStrokeWidth(onePath.fWidth);
-					canvas.drawPath(onePath, paint);
-					break;
-				case 4:
-					paint.setColor(Color.BLUE);
-					paint.setStrokeWidth(onePath.fWidth);
-					canvas.drawPath(onePath, paint);
-					break;
-			} 
+	public boolean onTouchEvent (MotionEvent event) 
+	{
+		synchronized (_thread.getSurfaceHolder()) {
+			if(event.getAction() == MotionEvent.ACTION_DOWN) {
+				path = new Path();
+				path.moveTo(event.getX(), event.getY());
+			} else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+				path.lineTo(event.getX(), event.getY());
+				_graphics.add(path);
+				path = new Path();
+				path.moveTo(event.getX(), event.getY());
+			} else if(event.getAction() == MotionEvent.ACTION_UP) {
+				path.lineTo(event.getX(), event.getY());
+				_graphics.add(path);
+			}
+			return true;
 		}
-    }
+	}
 
 	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-        	myPath = new MyPath();
-        	myPath.moveTo(event.getX(), event.getY());
-        	myPath.fWidth = mfPenWidth;
-        } else if(event.getAction() == MotionEvent.ACTION_MOVE) {
-        	myPath.lineTo(event.getX(), event.getY());
-        	path.add(myPath);
-        	myPath.color = miColor;
-            myPath = new MyPath();
-            myPath.moveTo(event.getX(), event.getY());
-            myPath.fWidth = mfPenWidth;
-        } else if(event.getAction() == MotionEvent.ACTION_UP) {
-        	myPath.lineTo(event.getX(), event.getY());
-        	path.add(myPath);
-        	myPath.fWidth = mfPenWidth;
-        }
-        invalidate();
-        return true;
-	}
-	
-	public void clear()
+	public void onDraw(Canvas canvas) 
 	{
-		path.clear();
-		invalidate();
+		for (Path path : _graphics) {
+			canvas.drawPath(path, mPaint);
+		}
 	}
+
+
+	public void surfaceCreated(SurfaceHolder holder) 
+	{
+		// TODO Auto-generated method stub
+		_thread.setRunning(true);
+		_thread.start();
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) 
+	{
+		 // TODO Auto-generated method stub	
+		 boolean retry = true;
+         _thread.setRunning(false);
+         while (retry) {
+             try {
+                 _thread.join();
+                 retry = false;
+             } catch (InterruptedException e) {
+                 // we will try it again and again...
+             }
+         }
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) 
+	{
+	 // TODO Auto-generated method stub
+	}
+}
+
+
+
+class DrawingThread extends Thread {
+     private SurfaceHolder _surfaceHolder;
+     private DrawView _panel;
+     private boolean _run = false;
+
+     public DrawingThread(SurfaceHolder surfaceHolder, DrawView panel) 
+     {
+         _surfaceHolder = surfaceHolder;
+         _panel = panel;
+     }
+
+     public void setRunning(boolean run) 
+     {
+         _run = run;
+     }
+
+     public SurfaceHolder getSurfaceHolder() 
+     {
+         return _surfaceHolder;
+     }
+
+     @Override
+     public void run() 
+     {
+         Canvas c;
+         while (_run) {
+             c = null;
+             try {
+                 c = _surfaceHolder.lockCanvas(null);
+                 synchronized (_surfaceHolder) {
+                     _panel.onDraw(c);
+                 }
+             } finally {
+                 // do this in a finally so that if an exception is thrown
+                 // during the above, we don't leave the Surface in an
+                 // inconsistent state
+                 if (c != null) {
+                     _surfaceHolder.unlockCanvasAndPost(c);
+                 }
+             }
+         }
+     }
 }
 
 class MyPath extends Path
